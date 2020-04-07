2f451a292859b4453f3ca4b64c64e4c9080e310d
fperdigon <fperdigon88@gmail.com>
2020-04-07 03:00:35 -0400

    Organize existing models and add the DL models that will be tested

--- deepFilter/dl_models.py
+++ deepFilter/dl_models.py
@@ -25,144 +25,298 @@ def NN_Model():
     #model.add(Dense(1, activation='sigmoid'))
     return model
 
-def deep_filter_model():
-    # TODO: Make the doc
+
+##########################################################################
+
+###### MODULES #######
+
+def LFilter_module(x, layers):
+    LB0 = Conv1D(filters=int(layers / 4),
+                 kernel_size=3,
+                 activation='linear',
+                 strides=1,
+                 padding='same')(x)
+    LB1 = Conv1D(filters=int(layers / 4),
+                kernel_size=5,
+                activation='linear',
+                strides=1,
+                padding='same')(x)
+    LB2 = Conv1D(filters=int(layers / 4),
+                kernel_size=9,
+                activation='linear',
+                strides=1,
+                padding='same')(x)
+    LB3 = Conv1D(filters=int(layers / 4),
+                kernel_size=15,
+                activation='linear',
+                strides=1,
+                padding='same')(x)
+
+
+    x = concatenate([LB1, LB1, LB2, LB3])
+
+    return x
+
+def NLFilter_module(x, layers):
+
+    NLB0 = Conv1D(filters=int(layers / 4),
+                  kernel_size=3,
+                  activation='relu',
+                  strides=1,
+                  padding='same')(x)
+    NLB1 = Conv1D(filters=int(layers / 4),
+                kernel_size=5,
+                activation='relu',
+                strides=1,
+                padding='same')(x)
+    NLB2 = Conv1D(filters=int(layers / 4),
+                kernel_size=9,
+                activation='relu',
+                strides=1,
+                padding='same')(x)
+    NLB3 = Conv1D(filters=int(layers / 4),
+                kernel_size=15,
+                activation='relu',
+                strides=1,
+                padding='same')(x)
+
+
+    x = concatenate([NLB0, NLB1, NLB2, NLB3])
+
+    return x
+
+def LANLFilter_module(x, layers):
+    LB0 = Conv1D(filters=int(layers / 8),
+                 kernel_size=3,
+                 activation='linear',
+                 strides=1,
+                 padding='same')(x)
+    LB1 = Conv1D(filters=int(layers / 8),
+                kernel_size=5,
+                activation='linear',
+                strides=1,
+                padding='same')(x)
+    LB2 = Conv1D(filters=int(layers / 8),
+                kernel_size=9,
+                activation='linear',
+                strides=1,
+                padding='same')(x)
+    LB3 = Conv1D(filters=int(layers / 8),
+                kernel_size=15,
+                activation='linear',
+                strides=1,
+                padding='same')(x)
+
+    NLB0 = Conv1D(filters=int(layers / 8),
+                  kernel_size=3,
+                  activation='relu',
+                  strides=1,
+                  padding='same')(x)
+    NLB1 = Conv1D(filters=int(layers / 8),
+                 kernel_size=5,
+                 activation='relu',
+                 strides=1,
+                 padding='same')(x)
+    NLB2 = Conv1D(filters=int(layers / 8),
+                 kernel_size=9,
+                 activation='relu',
+                 strides=1,
+                 padding='same')(x)
+    NLB3 = Conv1D(filters=int(layers / 8),
+                 kernel_size=15,
+                 activation='relu',
+                 strides=1,
+                 padding='same')(x)
+
+    x = concatenate([LB0, LB1, LB2, LB3, NLB0, NLB1, NLB2, NLB3])
+
+    return x
+
+def LANLFilter_module_dilated(x, layers):
+    LB1 = Conv1D(filters=int(layers / 6),
+                kernel_size=5,
+                activation='linear',
+                dilation_rate=3,
+                padding='same')(x)
+    LB2 = Conv1D(filters=int(layers / 6),
+                kernel_size=9,
+                activation='linear',
+                dilation_rate=3,
+                padding='same')(x)
+    LB3 = Conv1D(filters=int(layers / 6),
+                kernel_size=15,
+                dilation_rate=3,
+                activation='linear',
+                padding='same')(x)
+
+    NLB1 = Conv1D(filters=int(layers / 6),
+                 kernel_size=5,
+                 activation='relu',
+                 dilation_rate=3,
+                 padding='same')(x)
+    NLB2 = Conv1D(filters=int(layers / 6),
+                 kernel_size=9,
+                 activation='relu',
+                 dilation_rate=3,
+                 padding='same')(x)
+    NLB3 = Conv1D(filters=int(layers / 6),
+                 kernel_size=15,
+                 dilation_rate=3,
+                 activation='relu',
+                 padding='same')(x)
+
+    x = concatenate([LB1, LB2, LB3, NLB1, NLB2, NLB3])
+    # x = BatchNormalization()(x)
+
+    return x
+
+###### MODELS #######
+
+def deep_filter_vanilla_linear():
 
     model = Sequential()
-    model.add(Conv1D(filters=128,
-                     kernel_size=10,
+
+    model.add(Conv1D(filters=64,
+                     kernel_size=9,
                      activation='linear',
                      input_shape=(512, 1),
                      strides=1,
                      padding='same'))
     model.add(Conv1D(filters=64,
-                     kernel_size=10,
+                     kernel_size=9,
+                     activation='linear',
+                     strides=1,
+                     padding='same'))
+
+
+    model.add(Conv1D(filters=32,
+                     kernel_size=9,
                      activation='linear',
                      strides=1,
                      padding='same'))
     model.add(Conv1D(filters=32,
-                     kernel_size=10,
+                     kernel_size=9,
+                     activation='linear',
+                     strides=1,
+                     padding='same'))
+
+
+    model.add(Conv1D(filters=16,
+                     kernel_size=9,
                      activation='linear',
                      strides=1,
                      padding='same'))
     model.add(Conv1D(filters=16,
-                     kernel_size=10,
-                     activation='linear',
-                     strides=1,
-                     padding='same'))
-    model.add(Conv1D(filters=1,
-                     kernel_size=10,
+                     kernel_size=9,
                      activation='linear',
                      strides=1,
                      padding='same'))
 
+
+    model.add(Conv1D(filters=1,
+                     kernel_size=9,
+                     activation='linear',
+                     strides=1,
+                     padding='same'))
     return model
 
 
-def LANLFilter_module(x, layers):
-    LB = Conv1D(filters=int(layers/2),
-                kernel_size=10,
-                activation='linear',
-                strides=1,
-                padding='same')(x)
+def deep_filter_vanilla_relu():
+    model = Sequential()
 
-    NLB = Conv1D(filters=int(layers/2),
-                 kernel_size=10,
-                 activation='relu',
-                 strides=1,
-                 padding='same')(x)
-
-    x = concatenate([LB, NLB])
-    # x = BatchNormalization()(x)
-
-    return x
+    model.add(Conv1D(filters=64,
+                     kernel_size=9,
+                     activation='relu',
+                     input_shape=(512, 1),
+                     strides=1,
+                     padding='same'))
+    model.add(Conv1D(filters=64,
+                     kernel_size=9,
+                     activation='relu',
+                     strides=1,
+                     padding='same'))
 
 
-def LANLFilter_module_v3(x, layers):
-    LB1 = Conv1D(filters=int(layers / 6),
-                kernel_size=5,
-                activation='linear',
-                strides=1,
-                padding='same')(x)
-    LB2 = Conv1D(filters=int(layers / 6),
-                kernel_size=9,
-                activation='linear',
-                strides=1,
-                padding='same')(x)
-    LB3 = Conv1D(filters=int(layers / 6),
-                kernel_size=15,
-                activation='linear',
-                strides=1,
-                padding='same')(x)
+    model.add(Conv1D(filters=32,
+                     kernel_size=9,
+                     activation='relu',
+                     strides=1,
+                     padding='same'))
+    model.add(Conv1D(filters=32,
+                     kernel_size=9,
+                     activation='relu',
+                     strides=1,
+                     padding='same'))
 
-    NLB1 = Conv1D(filters=int(layers / 6),
-                 kernel_size=5,
-                 activation='relu',
-                 strides=1,
-                 padding='same')(x)
-    NLB2 = Conv1D(filters=int(layers / 6),
-                 kernel_size=9,
-                 activation='relu',
-                 strides=1,
-                 padding='same')(x)
-    NLB3 = Conv1D(filters=int(layers / 6),
-                 kernel_size=15,
-                 activation='relu',
-                 strides=1,
-                 padding='same')(x)
 
-    x = concatenate([LB1, LB2, LB3, NLB1, NLB2, NLB3])
-    # x = BatchNormalization()(x)
+    model.add(Conv1D(filters=16,
+                     kernel_size=9,
+                     activation='relu',
+                     strides=1,
+                     padding='same'))
+    model.add(Conv1D(filters=16,
+                     kernel_size=9,
+                     activation='relu',
+                     strides=1,
+                     padding='same'))
 
-    return x
 
-def LANLFilter_module_v4(x, layers):
-    LB1 = Conv1D(filters=int(layers / 6),
-                kernel_size=5,
-                activation='linear',
-                dilation_rate=3,
-                padding='same')(x)
-    LB2 = Conv1D(filters=int(layers / 6),
-                kernel_size=9,
-                activation='linear',
-                dilation_rate=3,
-                padding='same')(x)
-    LB3 = Conv1D(filters=int(layers / 6),
-                kernel_size=15,
-                dilation_rate=3,
-                activation='linear',
-                padding='same')(x)
+    model.add(Conv1D(filters=1,
+                     kernel_size=9,
+                     activation='linear',
+                     strides=1,
+                     padding='same'))
+    return model
 
-    NLB1 = Conv1D(filters=int(layers / 6),
-                 kernel_size=5,
-                 activation='relu',
-                 dilation_rate=3,
-                 padding='same')(x)
-    NLB2 = Conv1D(filters=int(layers / 6),
-                 kernel_size=9,
-                 activation='relu',
-                 dilation_rate=3,
-                 padding='same')(x)
-    NLB3 = Conv1D(filters=int(layers / 6),
-                 kernel_size=15,
-                 dilation_rate=3,
-                 activation='relu',
-                 padding='same')(x)
 
-    x = concatenate([LB1, LB2, LB3, NLB1, NLB2, NLB3])
-    # x = BatchNormalization()(x)
-
-    return x
-
-def deep_filter_model_v2():
-    # TODO: Make the doc
-
-    input_shape = (512, 1)  # I will assuame a 256x256x1 input shape
+def deep_filter_I_linear():
+    input_shape = (512, 1)
     input = Input(shape=input_shape)
 
-    tensor = LANLFilter_module(input, 128)
+    tensor = LFilter_module(input, 64)
+    tensor = LFilter_module(tensor, 64)
+    tensor = LFilter_module(tensor, 32)
+    tensor = LFilter_module(tensor, 32)
+    tensor = LFilter_module(tensor, 16)
+    tensor = LFilter_module(tensor, 16)
+    predictions = Conv1D(filters=1,
+                         kernel_size=10,
+                         activation='linear',
+                         strides=1,
+                         padding='same')(tensor)
+
+    model = Model(inputs=[input], outputs=predictions)
+
+
+def deep_filter_I_Nlinear():
+    input_shape = (512, 1)
+    input = Input(shape=input_shape)
+
+    tensor = NLFilter_module(input, 64)
+    tensor = NLFilter_module(tensor, 64)
+    tensor = NLFilter_module(tensor, 32)
+    tensor = NLFilter_module(tensor, 32)
+    tensor = NLFilter_module(tensor, 16)
+    tensor = NLFilter_module(tensor, 16)
+    predictions = Conv1D(filters=1,
+                         kernel_size=10,
+                         activation='linear',
+                         strides=1,
+                         padding='same')(tensor)
+
+    model = Model(inputs=[input], outputs=predictions)
+
+def deep_filter_I_LANL():
+    # TODO: Make the doc
+
+    input_shape = (512, 1)
+    input = Input(shape=input_shape)
+
+    tensor = LANLFilter_module(input, 64)
     tensor = LANLFilter_module(tensor, 64)
     tensor = LANLFilter_module(tensor, 32)
+    tensor = LANLFilter_module(tensor, 32)
+    tensor = LANLFilter_module(tensor, 16)
     tensor = LANLFilter_module(tensor, 16)
     predictions = Conv1D(filters=1,
                     kernel_size=10,
@@ -174,68 +328,23 @@ def deep_filter_model_v2():
 
     return model
 
-def deep_filter_model_v3():
+def deep_filter_model_I_LANL_dilated():
     # TODO: Make the doc
 
-    input_shape = (512, 1)  # I will assuame a 256x256x1 input shape
+    input_shape = (512, 1)
     input = Input(shape=input_shape)
 
-    tensor = LANLFilter_module_v3(input, 64)
-    tensor = LANLFilter_module_v3(tensor, 64)
-    tensor = LANLFilter_module_v3(tensor, 32)
-    tensor = LANLFilter_module_v3(tensor, 32)
-    tensor = LANLFilter_module_v3(tensor, 16)
-    tensor = LANLFilter_module_v3(tensor, 16)
+    tensor = LANLFilter_module(input, 64)
+    tensor = LANLFilter_module_dilated(tensor, 64)
+    tensor = LANLFilter_module(tensor, 32)
+    tensor = LANLFilter_module_dilated(tensor, 32)
+    tensor = LANLFilter_module(tensor, 16)
+    tensor = LANLFilter_module_dilated(tensor, 16)
     predictions = Conv1D(filters=1,
                     kernel_size=10,
                     activation='linear',
                     strides=1,
                     padding='same')(tensor)
-
-    model = Model(inputs=[input], outputs=predictions)
-
-    return model
-
-def deep_filter_model_v4():
-    # TODO: Make the doc
-
-    input_shape = (512, 1)  # I will assuame a 256x256x1 input shape
-    input = Input(shape=input_shape)
-
-    tensor = LANLFilter_module_v3(input, 64)
-    tensor = LANLFilter_module_v4(tensor, 64)
-    tensor = LANLFilter_module_v3(tensor, 32)
-    tensor = LANLFilter_module_v4(tensor, 32)
-    tensor = LANLFilter_module_v3(tensor, 16)
-    tensor = LANLFilter_module_v4(tensor, 16)
-    predictions = Conv1D(filters=1,
-                    kernel_size=10,
-                    activation='linear',
-                    strides=1,
-                    padding='same')(tensor)
-
-    model = Model(inputs=[input], outputs=predictions)
-
-    return model
-
-def deep_filter_model_v5():
-    # TODO: Make the doc
-
-    input_shape = (512, 1)  # I will assuame a 256x256x1 input shape
-    input = Input(shape=input_shape)
-
-    tensor = LANLFilter_module_v3(input, 64)
-    tensor = LANLFilter_module_v4(tensor, 64)
-    tensor = LANLFilter_module_v3(tensor, 32)
-    tensor = LANLFilter_module_v4(tensor, 32)
-    tensor = LANLFilter_module_v3(tensor, 16)
-    tensor = LANLFilter_module_v4(tensor, 16)
-    tensor = Conv1D(filters=1,
-                    kernel_size=10,
-                    activation='linear',
-                    strides=1,
-                    padding='same')(tensor)
-    predictions = BatchNormalization()(tensor)
 
     model = Model(inputs=[input], outputs=predictions)
 
