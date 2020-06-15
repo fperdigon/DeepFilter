1fc8fa27315f15324d6655ac4ccbae2c51c0dd24
fperdigon <fperdigon88@gmail.com>
2020-06-15 17:45:05 -0400

    Adding FCN_DAE comparative model. Adding Keras 1D Conv fuction.

--- deepFilter/dl_models.py
+++ deepFilter/dl_models.py
@@ -12,7 +12,27 @@
 
 import keras
 from keras.models import Sequential, Model
-from keras.layers import Dense, Conv1D, Flatten, Dropout, BatchNormalization, concatenate, Activation, Input
+from keras.layers import Dense, Conv1D, Flatten, Dropout, BatchNormalization, concatenate, Activation, Input, Conv2DTranspose, Lambda
+import keras.backend as K
+
+def Conv1DTranspose(input_tensor, filters, kernel_size, strides=2, activation='relu', padding='same'):
+    """
+        https://stackoverflow.com/a/45788699
+
+        input_tensor: tensor, with the shape (batch_size, time_steps, dims)
+        filters: int, output dimension, i.e. the output tensor will have the shape of (batch_size, time_steps, filters)
+        kernel_size: int, size of the convolution kernel
+        strides: int, convolution step size
+        padding: 'same' | 'valid'
+    """
+    x = Lambda(lambda x: K.expand_dims(x, axis=2))(input_tensor)
+    x = Conv2DTranspose(filters=filters,
+                        kernel_size=(kernel_size, 1),
+                        activation=activation,
+                        strides=(strides, 1),
+                        padding=padding)(x)
+    x = Lambda(lambda x: K.squeeze(x, axis=2))(x)
+    return x
 
 def NN_Model():
 
@@ -352,4 +372,128 @@ def deep_filter_model_I_LANL_dilated():
 
     model = Model(inputs=[input], outputs=predictions)
 
+    return model
+
+def FCN_DAE():
+    # Implementation of FCN_DAE approach presented in
+    # Chiang, H. T., Hsieh, Y. Y., Fu, S. W., Hung, K. H., Tsao, Y., & Chien, S. Y. (2019).
+    # Noise reduction in ECG signals using fully convolutional denoising autoencoders.
+    # IEEE Access, 7, 60806-60813.
+
+    input_shape = (512, 1)
+    input = Input(shape=input_shape)
+
+    x = Conv1D(filters=40,
+               input_shape=(512, 1),
+               kernel_size=16,
+               activation='elu',
+               strides=2,
+               padding='same')(input)
+
+    x = BatchNormalization()(x)
+
+    x = Conv1D(filters=20,
+               kernel_size=16,
+               activation='elu',
+               strides=2,
+               padding='same')(x)
+
+    x = BatchNormalization()(x)
+
+    x = Conv1D(filters=20,
+               kernel_size=16,
+               activation='elu',
+               strides=2,
+               padding='same')(x)
+
+    x = BatchNormalization()(x)
+
+    x = Conv1D(filters=20,
+               kernel_size=16,
+               activation='elu',
+               strides=2,
+               padding='same')(x)
+
+    x = BatchNormalization()(x)
+
+    x = Conv1D(filters=40,
+               kernel_size=16,
+               activation='elu',
+               strides=2,
+               padding='same')(x)
+
+    x = BatchNormalization()(x)
+
+    x = Conv1D(filters=1,
+               kernel_size=16,
+               activation='elu',
+               strides=1,
+               padding='same')(x)
+
+    x = BatchNormalization()(x)
+
+    # Keras has no 1D Traspose Convolution, instead we use Conv2DTranspose function
+    # in a souch way taht is mathematically equivalent
+    x = Conv1DTranspose(input_tensor=x,
+                        filters=1,
+                        kernel_size=16,
+                        activation='elu',
+                        strides=1,
+                        padding='same')
+
+    x = BatchNormalization()(x)
+
+    x = Conv1DTranspose(input_tensor=x,
+                        filters=40,
+                        kernel_size=16,
+                        activation='elu',
+                        strides=2,
+                        padding='same')
+
+    x = BatchNormalization()(x)
+
+    x = Conv1DTranspose(input_tensor=x,
+                        filters=20,
+                        kernel_size=16,
+                        activation='elu',
+                        strides=2,
+                        padding='same')
+
+    x = BatchNormalization()(x)
+
+    x = Conv1DTranspose(input_tensor=x,
+                        filters=20,
+                        kernel_size=16,
+                        activation='elu',
+                        strides=2,
+                        padding='same')
+
+    x = BatchNormalization()(x)
+
+    x = Conv1DTranspose(input_tensor=x,
+                        filters=20,
+                        kernel_size=16,
+                        activation='elu',
+                        strides=2,
+                        padding='same')
+
+    x = BatchNormalization()(x)
+
+    x = Conv1DTranspose(input_tensor=x,
+                        filters=40,
+                        kernel_size=16,
+                        activation='elu',
+                        strides=2,
+                        padding='same')
+
+    x = BatchNormalization()(x)
+
+    predictions = Conv1DTranspose(input_tensor=x,
+                        filters=1,
+                        kernel_size=16,
+                        activation='linear',
+                        strides=1,
+                        padding='same')
+
+    model = Model(inputs=[input], outputs=predictions)
     return model
