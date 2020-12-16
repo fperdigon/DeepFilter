17fd387acb5cb8bd7a0bd0cffd8ffd0f4a823c2f
fperdigon <fperdigon88@gmail.com>
2020-12-15 19:32:36 -0500

    Addind generate_table_time funtion to show per method processing time. Redability enhancement.

--- utils/visualization.py
+++ utils/visualization.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
-# conda install -c conda-forge prettytable
 from prettytable import PrettyTable
 
 def generate_violinplots(np_data, description, ylabel, log):
@@ -21,7 +20,6 @@ def generate_violinplots(np_data, description, ylabel, log):
     col = description
     loss_val_np = np.rot90(np_data)
     pd_df = pd.DataFrame.from_records(loss_val_np, columns=col)
-
 
     # Set up the matplotlib figure
 
@@ -33,7 +31,6 @@ def generate_violinplots(np_data, description, ylabel, log):
 
     if log:
         ax.set_yscale("log")
-        #ylabel = 'Log10( ' + ylabel + ' )'
 
     ax.set(xlabel='Models/Methods', ylabel=ylabel)
     ax = sns.despine(left=True, bottom=True)
@@ -49,7 +46,6 @@ def generate_barplot(np_data, description, ylabel, log):
     loss_val_np = np.rot90(np_data)
     pd_df = pd.DataFrame.from_records(loss_val_np, columns=col)
 
-
     # Set up the matplotlib figure
 
     f, ax = plt.subplots()
@@ -60,7 +56,6 @@ def generate_barplot(np_data, description, ylabel, log):
 
     if log:
         ax.set_yscale("log")
-        #ylabel = 'Log10( ' + ylabel + ' )'
 
     ax.set(xlabel='Models/Methods', ylabel=ylabel)
     ax = sns.despine(left=True, bottom=True)
@@ -69,12 +64,12 @@ def generate_barplot(np_data, description, ylabel, log):
 
     #plt.savefig(store_folder + 'violinplot_fco' + info + description + '.png')
 
+
 def generate_boxplot(np_data, description, ylabel, log):
     # Process the results and store in Panda objects
     col = description
     loss_val_np = np.rot90(np_data)
     pd_df = pd.DataFrame.from_records(loss_val_np, columns=col)
-
 
     # Set up the matplotlib figure
 
@@ -86,12 +81,9 @@ def generate_boxplot(np_data, description, ylabel, log):
 
     if log:
         ax.set_yscale("log")
-        #ylabel = 'Log10( ' + ylabel + ' )'
 
     ax.set(xlabel='Models/Methods', ylabel=ylabel)
     ax = sns.despine(left=True, bottom=True)
-
-
 
     plt.show()
 
@@ -105,7 +97,6 @@ def generate_hboxplot(np_data, description, ylabel, log, set_x_axis_size=None):
 
     pd_df = pd.DataFrame.from_records(loss_val_np, columns=col)
 
-
     # Set up the matplotlib figure
     sns.set(style="whitegrid")
 
@@ -115,16 +106,12 @@ def generate_hboxplot(np_data, description, ylabel, log, set_x_axis_size=None):
 
     if log:
         ax.set_xscale("log")
-        #ylabel = 'Log10( ' + ylabel + ' )'
 
     if set_x_axis_size != None:
         ax.set_xlim(set_x_axis_size)
 
-
     ax.set(ylabel='Models/Methods', xlabel=ylabel)
     ax = sns.despine(left=True, bottom=True)
-
-
 
     plt.show()
 
@@ -149,38 +136,6 @@ def ecg_view(ecg, ecg_blw, ecg_dl, ecg_f, signal_name=None, beat_no=None):
         plt.title('Signal ' + str(signal_name) + 'beat ' + str(beat_no))
     else:
         plt.title('ECG signal for comparison')
-
-    plt.show()
-
-def ecg_view_2(ecg, ecg_blw, ecg_dl, signal_name=None, beat_no=None):
-
-    fig, ax = plt.subplots(figsize=(16, 9))
-    plt.subplot(311)
-    plt.title('ECG original signal')
-    plt.plot(ecg, 'g', label='ECG orig')
-    plt.grid(True)
-    plt.ylabel('au')
-    plt.xlabel('samples')
-    plt.subplots_adjust(hspace=0.5)
-
-
-    plt.subplot(312)
-    plt.title('ECG original + BLW')
-    plt.plot(ecg_blw, 'k', label='ECG + BLW')
-    plt.grid(True)
-    plt.ylabel('au')
-    plt.xlabel('samples')
-    plt.subplots_adjust(hspace=0.5)
-
-    plt.subplot(313)
-    plt.title('Filtered ECG + BLW signal using DeepFilter')
-    plt.plot(ecg_dl, 'b', label='ECG DL Filtered')
-    plt.plot(ecg - ecg_dl, color='#CB9590', lw=2, label='Difference ECG - DeepFilter')
-    plt.grid(True)
-    plt.ylabel('au')
-    plt.xlabel('samples')
-    plt.legend()
-    plt.subplots_adjust(hspace=0.5)
 
     plt.show()
 
@@ -210,7 +165,6 @@ def ecg_view_diff(ecg, ecg_blw, ecg_dl, ecg_f, signal_name=None, beat_no=None):
 
 def generate_table(metrics, metric_values, Exp_names):
     # Print tabular results in the console, in a pretty way
-
     print('\n')
 
     tb = PrettyTable()
@@ -223,7 +177,7 @@ def generate_table(metrics, metric_values, Exp_names):
         tb_row = []
         tb_row.append(exp_name)
 
-        for metric in metric_values:
+        for metric in metric_values:   # metric_values[metric][model][beat]
             m_mean = np.mean(metric[ind])
             m_std = np.std(metric[ind])
             tb_row.append('{:.3f}'.format(m_mean) + ' (' + '{:.3f}'.format(m_std) + ')')
@@ -233,7 +187,8 @@ def generate_table(metrics, metric_values, Exp_names):
 
     print(tb)
 
-def generate_table_time(column_names, all_values, Exp_names):
+
+def generate_table_time(column_names, all_values, Exp_names, gpu=True):
     # Print tabular results in the console, in a pretty way
 
     # The FIR and IIR are the last on all_values
@@ -246,9 +201,14 @@ def generate_table_time(column_names, all_values, Exp_names):
     tb = PrettyTable()
     ind = 0
 
-    for exp_name in Exp_names:
+    if gpu:
+        device = 'GPU'
+    else:
+        device = 'CPU'
 
-        tb.field_names = ['Method/Model'] + [column_names[0] + '(GPU) h:m:s:ms'] + [column_names[1] + '(GPU) h:m:s:ms']
+    for exp_name in Exp_names:
+        tb.field_names = ['Method/Model'] + [column_names[0] + '(' + device + ') h:m:s:ms'] + [
+            column_names[1] + '(' + device + ') h:m:s:ms']
 
         tb_row = []
         tb_row.append(exp_name)
@@ -260,3 +220,6 @@ def generate_table_time(column_names, all_values, Exp_names):
         ind += 1
 
     print(tb)
+
+    if gpu:
+        print('* For FIR and IIR Filters is CPU since scipy filters are CPU based implementations')
