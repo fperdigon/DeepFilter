73442050241743232b5f725eabf38e3cea16a153
fperdigon <fperdigon88@gmail.com>
2021-05-13 10:34:10 -0400

    Improving the NSTDB preparation.

--- Data_Preparation/Prepare_NSTDB.py
+++ Data_Preparation/Prepare_NSTDB.py
@@ -25,16 +25,22 @@ import numpy as np
 import wfdb
 import _pickle as pickle
 
-def prepare(NSTDBPath='data/mit-bih-noise-stress-test-database-1.0.0/bw'):
-    signals, fields = wfdb.rdsamp(NSTDBPath)
+def prepare(NSTDBPath='data/mit-bih-noise-stress-test-database-1.0.0/'):
 
-    for key in fields:
-        print(key, fields[key])
 
-    np.save('data/NoiseBWL', signals)
+    bw_signals, bw_fields = wfdb.rdsamp(NSTDBPath + 'bw')
+    em_signals, em_fields = wfdb.rdsamp(NSTDBPath + 'em')
+
+
+    for key in bw_fields:
+        print(key, bw_fields[key])
+
+    for key in em_fields:
+        print(key, em_fields[key])
+
     # Save Data
     with open('data/NoiseBWL.pkl', 'wb') as output:  # Overwrites any existing file.
-        pickle.dump(signals, output)
+        pickle.dump([bw_signals, em_signals], output)
     print('=========================================================')
     print('MIT BIH data noise stress test database (NSTDB) saved as pickle')
 
